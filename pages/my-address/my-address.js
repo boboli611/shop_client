1f710ea528d9b9d967649d185a00094cb42b963c
lvferry <232149615@qq.com>
2018-03-25 18:42:59 +0800

    add：我的地址

---
+++ pages/my-address/my-address.js
@@ -0,0 +1,71 @@
+// pages/my-address/my-address.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    state: "complete", // "edit": 编辑状态, "complete": 浏览状态
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  switchPageState(){
+    this.setData({
+      state: this.data.state === 'complete'? 'edit' : 'complete'
+    })
+  }
+})
