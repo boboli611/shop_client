7bc0ba305e54563ed3011a818b813f6c2bfe6ffe
lvferry <232149615@qq.com>
2018-03-28 17:22:16 +0800

    add：cart

---
+++ pages/cart/cart.js
@@ -0,0 +1,71 @@
+// pages/cart/cart.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    state: 'normal', //'normal': 正常状态, 'edit': 编辑
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  switchPageState(){
+    this.setData({
+      state: this.data.state === 'normal'? 'edit' : 'normal'
+    })
+  }
+})
