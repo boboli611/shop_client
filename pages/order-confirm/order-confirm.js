8c410665551be858c2de5bd3358c2c015558962c
lvferry <232149615@qq.com>
2018-03-29 17:22:21 +0800

    确认订单

--- pages/order-confirm/order-confirm.js
+++ pages/order-confirm/order-confirm.js
@@ -1,76 +1,66 @@
-// pages/cart/cart.js
+// pages/order-confirm/order-confirm.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    state: 'normal', //'normal': 正常状态, 'edit': 编辑
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
-  },
-  switchPageState(){
-    this.setData({
-      state: this.data.state === 'normal'? 'edit' : 'normal'
-    })
-  },
-  pay(){
-    wx.navigateTo({
-      url: '../order-confirm/order-confirm'
-    })
+  
   }
 })
